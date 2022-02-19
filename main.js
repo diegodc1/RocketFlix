fccb65570de4292a9d2e0a46a2b85b224e40c5fe
Diego Alves <diegoalvesdc12@gmail.com>
2022-02-19 16:52:58 -0300

    Third Commit

--- main.js
+++ main.js
@@ -1,26 +1,49 @@
+const image = document.getElementById("movieImage");
+const average = document.getElementById("averageNote")
+const movieDate = document.getElementById("movieDate")
+const infoDetails = document.getElementById("infoDetails") 
+
+
 const API_KEY = 'api_key=0c0cd73908db86277cdea9e6ec945b6d';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
-const language = 'language=pt-BR';
-const API_URL = "/discover/movie?sort_by=popularity.desc&";
-
-const url = BASE_URL + API_URL + API_KEY 
+const language = '&language=pt-BR';
+const API_URL = "/movie/top_rated?";
+const url = BASE_URL + API_URL + API_KEY + language
 
 function getMovie() {
-  const number = Math.floor(Math.random() * (20 - 1 )) + 1
+  const number = randomNumber()
+  
   const data = axios.get(url)
   .then(response => {
-    const checkInfo = response.data.results
+
+    const check = response.data.results
     const titleMovie = response.data.results[number].title;
-    const DescriptionMovie = response.data.results[number].overview
-    const movieImage = response.data.results[number].poster_path
-    const imageUrl = IMG_URL + movieImage
+    const descriptionMovie = response.data.results[number].overview
+    const getImage = response.data.results[number].poster_path
+    const imageUrl = IMG_URL + getImage
+    const averageRating = response.data.results[number].vote_average
+    const releaseData = response.data.results[number].release_date
+
     movieTitle.textContent = titleMovie;
-    movieDescription.textContent = DescriptionMovie;
-    document.getElementById("movieImage").src = imageUrl;
-  })
+    movieDescription.textContent = descriptionMovie;
+    average.textContent = averageRating
+    movieDate.textContent = releaseData.substr(0, 4)
+
+    image.src = imageUrl;
+    image.classList.remove("invisible");
+    infoDetails.classList.remove("invisible");
+
+
+    console.log(check)
+    })
   .catch(error => {console.log(error);
   })
+}
+
+function randomNumber(){
+  const number = Math.floor(Math.random() * (20 - 0 )) + 0;
+  return number
 }
 
 
